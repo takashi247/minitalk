d1bf99ae292763c7378afecdce45430b6ec400ca
takashi247 <takashi247@gmail.com>
2021-07-12 19:04:48 +0900

    add minitalk.c and minitalk.h

---
+++ minitalk.c
@@ -0,0 +1,34 @@
+#include <signal.h>
+#include <unistd.h>
+#include "minitalk.h"
+
+size_t
+    ft_strlen(const char *s)
+{
+    size_t  len;
+
+    len = 0;
+    while (s[len])
+        len++;
+    return (len);
+}
+
+static void
+    sig_usr(int signo)
+{
+    if (signo == SIGUSR1)
+        write(STDOUT_FILENO, MSG_USR1_SUCCESS, ft_strlen(MSG_USR1_SUCCESS));
+    else if (signo == SIGUSR2)
+        write(STDOUT_FILENO, MSG_USR2_SUCCESS, ft_strlen(MSG_USR2_SUCCESS));
+}
+
+int
+    main(void)
+{
+    if (signal(SIGUSR1, sig_usr) == SIG_ERR)
+        write(STDERR_FILENO, MSG_USR1_FAILURE, ft_strlen(MSG_USR1_FAILURE));
+    if (signal(SIGUSR2, sig_usr) == SIG_ERR)
+        write(STDERR_FILENO, MSG_USR2_FAILURE, ft_strlen(MSG_USR2_FAILURE));
+    for ( ; ; )
+        pause();
+}
---
+++ minitalk.h
@@ -0,0 +1,9 @@
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# define MSG_USR1_SUCCESS "received SIGUSR1\n"
+# define MSG_USR2_SUCCESS "received SIGUSR2\n"
+# define MSG_USR1_FAILURE "can't catch SIGUSR1\n"
+# define MSG_USR2_FAILURE "can't catch SIGUSR2\n"
+
+#endif
