3448987ea0428cf5dc4a96fa9312924de097a257
takashi247 <takashi247@gmail.com>
2021-07-28 23:23:58 +0900

    modify server/client to enable short string communications

--- client.h
+++ client.h
@@ -12,9 +12,12 @@
 # define CHAR_ONE '1'
 # define FAILURE 1
 # define SUCCESS 0
+# define MSG_USR1_FAILURE "can't catch SIGUSR1\n"
+# define MSG_USR2_FAILURE "can't catch SIGUSR2\n"
 # define MSG_USAGE_ERROR "Bad usage. Specify a server PID and string"
 # define MSG_BIN_STR_ERROR "Error: only 0 and 1 are allowed to use"
 # define MSG_SERVER_PID_ERROR "Error: server pid is not correct"
+# define MSG_RECEIVE_ACKNOWLEDGEMENT "Success: receive acknowledgement"
 # define STR_BIN_NULL "00000000"
 
 #endif
